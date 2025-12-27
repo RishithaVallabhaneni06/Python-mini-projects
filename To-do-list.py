d07f9c6ef005f99ed376f262fcff65d2787b73ea
Rishitha Chowdary Vallabhaneni <141801275+RishithaVallabhaneni06@users.noreply.github.com>
2025-12-27 21:46:13 +0530

    To-do-list.py

--- To-do-list.py
+++ To-do-list.py
@@ -1,37 +1,42 @@
 print("SIMPLE TO-DO LIST\n")
-task={}
-while(True):
+
+task = {}
+
+while True:
     print("1.Add Task\t 2.View Tasks\t 3.Finish Task\t 4.Remove Task\t 5.Exit")
-    inp=int(input("Enter your Choice:"))
-    if inp==1:
-        name=input("Enter task name:")
-        task[name]="pending"
+    inp = int(input("Enter your Choice: "))
+
+    if inp == 1:
+        name = input("Enter task name: ")
+        task[name] = "Pending"
         print("Task Added Successfully")
-    if inp==2:
-        name=input("Enter task name:")
+
+    elif inp == 2:
+        if not task:
+            print("No tasks available")
+        else:
+            for name, status in task.items():
+                print(name, "->", status)
+
+    elif inp == 3:
+        name = input("Enter task name: ")
         if name in task:
-            print(task)
+            task[name] = "Finished"
+            print("Task marked as Done ✅")
         else:
             print("Task doesn't exist")
-    elif inp==3:
-        name=input("Enter the task name to mark as finished :")
+
+    elif inp == 4:
+        name = input("Enter task name: ")
         if name in task:
-            task[name]="Finished"
-            print("Task marked as Done✅")
-        else:
-            print("Task doesn't exist")
-    elif inp==4:
-        name=input("Enter task name:")
-        if name in task:
-            del(task[name])
+            del task[name]
             print("Task Deleted Successfully")
         else:
             print("Task doesn't exist")
-    elif inp==5:
-        print("Thank You")
+
+    elif inp == 5:
+        print("Thank You ")
         break
+
     else:
         print("Invalid Choice")
-    
-        
-        
